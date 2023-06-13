a6ff5b9bbab617f14640e84f9cd03b36f99de9f0
winniewandia <winniewandia63@gmail.com>
2023-06-13 10:10:10 +0300

    Introduced a var and checks if the number is positive or negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number % 10 > 5:
-    print(f"Last digit of {number} is {number % 10} and is greater than 5")
-elif number % 10 == 0:
-    print(f"Last digit of {number} is {number % 10} and is 0")
-elif number % 10 < 6 and number % 10 != 0:
+result = number % 10 if number >= 0 else -number % 10
+if number < 0:
+    result = -result
+if result > 5:
+    print(f"Last digit of {number} is {result} and is greater than 5")
+elif result == 0:
+    print(f"Last digit of {number} is {result} and is 0")
+elif result < 6 and result != 0:
     print(
-        f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
+        f"Last digit of {number} is {result} and is less than 6 and not 0")
