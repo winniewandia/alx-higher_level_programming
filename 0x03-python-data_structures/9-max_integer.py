e79cb81c7340fc20c49f4a5d64f76cafaf020edd
winniewandia <winniewandia63@gmail.com>
2023-06-14 12:10:05 +0300

    Included last element in list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
         return None
     else:
         max = my_list[0]
-        for i in range(1, length - 1):
+        for i in range(1, length):
             if my_list[i] > max:
                 max = my_list[i]
         return max
