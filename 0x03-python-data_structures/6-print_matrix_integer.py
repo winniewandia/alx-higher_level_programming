fc31f331c5443f3c6e7b25648c812fad4680d683
winniewandia <winniewandia63@gmail.com>
2023-06-14 10:49:31 +0300

    removed space at the end of each row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    new = []
-    for i in matrix:
-        new = i
-        for j in new:
-            print("{:d}".format(j), end=' ')
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            if col != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[row][col]), end='')
         print()
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
