2c9c7ef410c9928a8eb62ff609a00b2f8b28df1a
winniewandia <winniewandia63@gmail.com>
2023-06-14 18:53:22 +0300

    Used max func

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,9 +2,4 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    keys = a_dictionary.keys()
-    max = 0
-    for k in keys:
-        if a_dictionary[k] > max:
-            max = a_dictionary[k]
-    return k
+    return (max(a_dictionary, key=a_dictionary.get))
