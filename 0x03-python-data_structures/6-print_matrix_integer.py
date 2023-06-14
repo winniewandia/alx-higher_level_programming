79655a04e0bd616a1da4170aa27693e1e697882e
winniewandia <winniewandia63@gmail.com>
2023-06-14 10:49:54 +0300

    Removed test code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,14 +6,3 @@ def print_matrix_integer(matrix=[[]]):
                 print(" ", end='')
             print("{:d}".format(matrix[row][col]), end='')
         print()
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
