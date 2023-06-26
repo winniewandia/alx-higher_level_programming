144bc649fd70ac90bf888559da8449b2a3112a18
winniewandia <winniewandia63@gmail.com>
2023-06-26 18:31:09 +0300

    tried using raise after printing message for the exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,10 +8,24 @@ def safe_print_list_integers(my_list=[], x=0):
         except TypeError:
             pass
         except IndexError:
-            pass
+            print("list index out of range")
+            raise
         except ValueError:
             pass
         else:
             count += 1
     print()
     return count
+
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
