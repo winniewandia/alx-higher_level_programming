1f3ac1f0e5eaae2353a52bedfc3edf915264c6aa
winniewandia <winniewandia63@gmail.com>
2023-06-27 19:37:37 +0300

    added docstring for position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,6 +15,7 @@ class Square:
         """This initializes the size of the square
         Attributes:
         size(int): The size of the square
+        position(int): Position of the square
         """
         self.size = size
         self.position = position
@@ -40,7 +41,7 @@ class Square:
     def position(self):
         """This method gets the position property.
         The setter method below sets the value of position
-        depending on whether its a tuple of 2 int
+        depending on whether its a tuple of 2 positive integers
         """
         return self.__position
 
