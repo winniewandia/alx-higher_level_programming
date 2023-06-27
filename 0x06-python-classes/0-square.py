d75a8c0e1bff7553881fa871ca7936ceb3bce773
winniewandia <winniewandia63@gmail.com>
2023-06-27 15:14:49 +0300

    added the doc string

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 class Square:
-    """Class Square that defines a square with 4 equal sides
-    """
+    """Class Square that defines a square with 4 equal sides.
+    There are no Attributes yet"""
     pass
