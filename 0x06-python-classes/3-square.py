19169360d24cf356c9d76e5ac6eb2aa502fd9801
winniewandia <winniewandia63@gmail.com>
2023-06-27 17:43:18 +0300

    class square with a method that computes the area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module is called 2-square.
+
+It contains a class Square with private size attribute
+that raises TypeError and ValueError
+"""
+
+
+class Square:
+    """The class Square defines a square with
+    private instance size.
+    """
+
+    def __init__(self, size=0):
+        """This initializes the size of the square
+        Attributes:
+        size(int): The size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(size)
+
+    def area(self):
+        """This method computes the area of a square
+        using its size
+        """
+        return int(self.__size) * int(self.__size)
