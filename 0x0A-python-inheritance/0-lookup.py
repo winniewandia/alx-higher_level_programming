e685a3ccbd151bd592a41d7c5c873499b3effbd7
winniewandia <winniewandia63@gmail.com>
2023-07-10 17:37:08 +0300

    function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is the Lookup module that contains
+the lookup function
+"""
+
+
+def lookup(obj):
+    """function that returns the list of available attributes
+    and methods of an object
+
+    Args:
+        obj (object): instance of the class
+
+    Returns:
+        Returns a list object
+    """
+    return dir(object)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+Inheritance is the capability of one class to derive or inherit the properties from another class
+In python, every class inherits from a builtin basic class called 'object'
