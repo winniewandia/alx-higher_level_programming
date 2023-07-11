437531fe33a17d3683896f1d744dc6b1b216b74b
winniewandia <winniewandia63@gmail.com>
2023-07-11 17:34:58 +0300

    added documentation

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""This module contains the append_write function
+"""
+
+
 def append_write(filename="", text=""):
+    """function that appends a string at the end of a
+    text file (UTF8) and returns the number of characters added
+
+    Args:
+        filename (str, optional): name of file. Defaults to "".
+        text (str, optional): text to be appended. Defaults to "".
+
+    Returns:
+        number of bytes: bytes appended
+    """
     with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
