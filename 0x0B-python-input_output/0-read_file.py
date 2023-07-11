34c64fe54dfac0382059f567fafa47a878815746
winniewandia <winniewandia63@gmail.com>
2023-07-11 17:11:57 +0300

    added documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""This module contains a read_file function
+"""
+
+
 def read_file(filename=""):
+    """a function that reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename (str, optional): name of file to be read Defaults to "".
+    """
     with open(filename) as f:
         for line in f:
             print(line)
