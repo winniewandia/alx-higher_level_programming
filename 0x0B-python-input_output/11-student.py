ecb2bd31dcfce94bb638080e707c4c6aca286720
winniewandia <winniewandia63@gmail.com>
2023-07-12 15:15:26 +0300

    class student with method that replaces all attributes of the Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""This module contains the class Student
+"""
+
+
+class Student:
+    """The class Student contains instatiation and public method
+    to_json
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age
+
+        Args:
+            first_name: first name
+            last_name: last name
+            age: age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+
+        Returns:
+            dictionary representation of a Student instance
+        """
+        src = self.__dict__.copy()
+        if isinstance(attrs, list):
+
+            for item in attrs:
+                if not isinstance(item, str):
+                    return src
+
+            my_list = {}
+
+            for i in range(len(attrs)):
+                for j in src:
+                    if attrs[i] == j:
+                        my_list[j] = src[j]
+            return my_list
+
+        return src
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance
+
+        Args:
+            json: a dictionary
+        """
+        for attr in json:
+            self.__dict__[attr] = json[attr]
