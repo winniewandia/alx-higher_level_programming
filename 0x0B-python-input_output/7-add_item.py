789f53cca538a0142b833df2d6f838da406cd592
winniewandia <winniewandia63@gmail.com>
2023-07-12 14:39:27 +0300

    documented module

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list,
-and then save them to a file
+"""This module contains a script that adds all arguments to a
+Python list, and then save them to a file
 """
 import sys
 import os.path
