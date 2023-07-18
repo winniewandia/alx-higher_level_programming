655041e61d90c62e611c271bff881835e4b336d4
winniewandia <winniewandia63@gmail.com>
2023-07-18 13:43:22 +0300

    return string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,5 +33,5 @@ class Base:
             Json string: JSON string representation of list_dictionaries
         """
         if list_dictionaries is None or list_dictionaries == "[]":
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
