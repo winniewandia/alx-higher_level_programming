015d6093354a316d53aaa53768fffc7f6bfb78b5
winniewandia <winniewandia63@gmail.com>
2023-09-11 14:02:47 +0300

    script that prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const lang1 = 'C is fun';
+const lang2 = 'Python is cool';
+const lang3 = 'JavaScript is amazing';
+console.log(lang1);
+console.log(lang2);
+console.log(lang3);
--- 0x12-javascript-warm_up/README.md
+++ 0x12-javascript-warm_up/README.md
@@ -1,1 +1,2 @@
 0. Create a const variable and print it using console.log
+1. script that prints 3 lines
