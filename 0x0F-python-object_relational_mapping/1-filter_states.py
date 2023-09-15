5209127505e1c0a93c2faa120ab9ac4e73416607
winniewandia <winniewandia63@gmail.com>
2023-09-15 10:48:28 +0300

    lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '%N%' ORDER BY id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
--- 0x0F-python-object_relational_mapping/README.md
+++ 0x0F-python-object_relational_mapping/README.md
@@ -1,1 +1,3 @@
 0. lists all states from the database hbtn_0e_0_usa
+1. lists all states with a name starting with N (upper N)
+   from the database hbtn_0e_0_usa
