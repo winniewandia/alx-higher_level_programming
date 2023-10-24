9d8634371b2110833e699465256b2b509f14b246
winniewandia <winniewandia63@gmail.com>
2023-10-24 13:42:41 +0300

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (error, body) {
-    if (body == undefined) {
-        console.log(error);
-    } else {
-        console.log(body);
-    }
+  if (body === undefined) {
+    console.log(error);
+  } else {
+    console.log(body);
+  }
 });
